292a52b7a0d7382cdad39d5eff6f04cf752af637
ThinkStrive <infothinkstrive@gmail.com>
2024-10-25 07:41:45 +0530

    updated

--- src/components/reuse/auth/Login.jsx
+++ src/components/reuse/auth/Login.jsx
@@ -56,32 +56,32 @@ const Login = ({ inputData, setInputData }) => {
   return (
     <div className=" flex flex-col gap-5 justify-center items-center">
       <div
-        className="bg-[#040404] shadow-2xl 2xl:h-[50rem] lg:h-[70vh] lg:w-[33vw] md:h-[65vh] md:w-[55vw] h-[75vh] w-[85vw] rounded-3xl px-10 py-6 flex flex-col justify-center gap-5"
+        className="bg-[#040404] shadow-2xl 2xl:h-[50rem] border-2 lg:h-[70vh] lg:w-[33vw] md:h-[65vh] md:w-[55vw] h-[55vh] w-[85vw] rounded-3xl lg:px-10 px-7 py-6 flex flex-col justify-center gap-5"
         // style={{ backgroundColor: "rgb(245,245,245)" }}
       >
-        <h2 className="text-white lg:text-4xl md:text-wl text-lg font-medium text-center my-3">
+        <h2 className="text-white lg:text-4xl md:text-3xll text-2xl font-medium text-center lg:my-3">
           Login
         </h2>
 
         <form
           action=""
           onSubmit={handleAuthLoginButton}
-          className="flex flex-col justify-between items-start h-[53%] w-full mt-3"
+          className="flex flex-col justify-between items-start lg:h-[53%] h-[55%] w-full lg:mt-3 mt-2"
         >
           <input
             type="email"
             placeholder="Enter your Email"
-            className="bg-slate-300 outline-none lg:w-[100%] w-[95%] shadow-lg px-4 py-4 rounded-lg text-sm lg:text-[16px] login-input text-black placeholder:text-slate-700 font-bold"
+            className="bg-slate-300 outline-none lg:w-[100%] w-[100%] shadow-lg px-4 lg:py-4 py-3 rounded-lg text-sm lg:text-[16px] login-input text-black placeholder:text-slate-700 font-bold"
             name="email"
             id="email"
             value={inputData.email}
             onChange={handleAuthLoginInputChange}
           />
-          <div className="relative lg:w-[100%] w-[95%]">
+          <div className="relative lg:w-[100%] w-[100%]">
             <input
               type={`${isPasswordView ? "text" : "password"}`}
               placeholder="Enter your password"
-              className="bg-slate-300 outline-none lg:w-[100%] w-[95%] shadow-lg px-4 py-4 rounded-lg text-sm lg:text-[16px] login-input text-black placeholder:text-slate-700 font-bold"
+              className="bg-slate-300 outline-none lg:w-[100%] w-[100%] shadow-lg px-4 lg:py-4 py-3 rounded-lg text-sm lg:text-[16px] login-input text-black placeholder:text-slate-700 font-bold"
               name="password"
               id="password"
               value={inputData.password}
@@ -97,14 +97,14 @@ const Login = ({ inputData, setInputData }) => {
           {loading ? (
             <button
               type="submit"
-              className={`w-[100%] bg-darkNavy py-3 shadow-2xl rounded-lg text-lg font-semibold text-white h-[55px] flex justify-center items-center`}
+              className={`w-[100%] bg-darkNavy py-3 shadow-2xl rounded-lg text-lg font-semibold text-white lg:h-[55px] h-[50px] flex justify-center items-center`}
             >
               <div className="login-loader"></div>
             </button>
           ) : (
             <button
               type="submit"
-              className={`w-[100%] bg-darkNavy py-3 shadow-2xl rounded-lg text-lg font-semibold text-white h-[55px]`}
+              className={`w-[100%] bg-darkNavy py-3 shadow-2xl rounded-lg text-lg font-semibold text-white lg:h-[55px] h-[50px]`}
               // style={{ backgroundColor: "rgb(239,68,68)" }}
             >
               Login
@@ -112,7 +112,7 @@ const Login = ({ inputData, setInputData }) => {
           )}
         </form>
 
-        <p className="text-sm m-0 text-white">
+        <p className="lg:text-sm text-xs m-0 text-white">
           Don't Have An Account ?{" "}
           <Link to="/auth/register" className="cursor-pointer text-blue-500">
             Sign up
